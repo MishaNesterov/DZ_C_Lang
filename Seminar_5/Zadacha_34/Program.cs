2e5d28a41ba2df4b63eea006e43d82e1295d53e1
Misha <misha_n@mail.com>
2023-02-06 19:45:14 +0300

    Задание 34, 38_2

---
+++ Seminar_5/Zadacha_34/Program.cs
@@ -0,0 +1,35 @@
+﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами.
+// Напишите программу, которая покажет количество чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+
+int InPut(string input)
+{
+    Console.Write(input);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+void Array(int []a)
+{
+    for(int i = 0; i < a.Length; i++ )
+    {
+        a[i] = new Random().Next(100, 1000);      
+    } 
+}
+
+void WriteArray(int[] a, int k = 0)
+{
+    Console.Write("[");
+    for (int i = 0; i < a.Length; i++)
+    {
+        if (i < (a.Length - 1)) Console.Write(a[i] + ", ");       
+        else Console.Write(a[i] + "]");       
+        if (a[i] % 2 == 0) k++; 
+    }
+    Console.Write(" -> " + k);
+}
+
+int n = InPut("Введите количество элементов в массиве: ");
+int size = n;
+int[] a = new int[size];
+Array(a);
+WriteArray(a);
